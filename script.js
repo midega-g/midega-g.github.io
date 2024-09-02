138b42b1e0d874257896750b95c6a3848d2fb10e
George Midega <67196846+Demiga-g@users.noreply.github.com>
2024-09-02 23:22:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function smoothScroll(target) {
+    $('html, body').animate({
+        scrollTop: $(target).offset().top - 100
+    }, 1000);
+}
+
+$(document).ready(function () {
+    var heroSection = $('.hero-section');
+    var heroSectionHeight = heroSection.height();
+    var header = $('.sticky-header');
+    var headerHeight = header.height();
+    var currentPage = window.location.pathname.split("/").pop();
+
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll > heroSectionHeight + headerHeight) {
+            header.css('transform', 'translateY(-100%)');
+        } else {
+            header.css('transform', 'translateY(0)');
+        }
+    });
+
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        smoothScroll(target);
+    });
+
+    $('nav ul li a').each(function() {
+        var linkPage = $(this).attr('href');
+        if (currentPage === linkPage || (currentPage === "" && linkPage === "index.html")) {
+            $(this).addClass('active');
+        }
+    });
+
+    $('.hamburger-menu').on('click', function() {
+        $('.nav-list').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    // close menu when a link is clicked
+    $('.nav-list a').on('click', function() {
+        $('.nav-list').removeClass('active');
+        $('.hamburger-menu').removeClass('active');
+    });
+
+    //close menu when clicking outside
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.sticky-header').length) {
+            $('.nav-list').removeClass('active');
+            $('.hamburger-menu').removeClass('active');
+        }
+    });
+
+    $(window).off('scroll');
+
+    $('#current-year').text(new Date().getFullYear());
+});
